03153897d88a61c36949b53045ac549f9653b68a
maher <mahersamy699@yahoo.com>
2024-03-22 06:01:17 +0200

    create meal item

--- lib/features/menu/presentions/widgets/meal_item.dart
+++ lib/features/menu/presentions/widgets/meal_item.dart
@@ -7,8 +7,57 @@ class MealItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-
+    return Row(
+      children: [
+        Image.network(
+          "https://img.freepik.com/free-psd/delicious-fried-chicken-with-french-fries-isolated-transparent-background_191095-11719.jpg?t=st=1711058653~exp=1711062253~hmac=f104b99d6c6b9f53a6d74a44bf55e936bf5c2cd7536b48e382b592a1b3bd9f47&w=740",
+          height: 50.h,
+          width: 50.w,
+        ),
+        SizedBox(
+          width: 16.h,
+        ),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            Text(
+              "Fried Chicken",
+              style: Theme.of(context)
+                  .textTheme
+                  .displayMedium!
+                  .copyWith(color: AppColors.black),
+            ),
+            SizedBox(
+              height: 8.h,
+            ),
+            Text(
+              "Checken hint",
+              style: Theme.of(context)
+                  .textTheme
+                  .displaySmall!
+                  .copyWith(color: AppColors.grey),
+            ),
+            SizedBox(
+              height: 8.h,
+            ),
+            Text(
+              "200 LE",
+              style: Theme.of(context)
+                  .textTheme
+                  .displaySmall!
+                  .copyWith(color: AppColors.grey),
+            ),
+          ],
+        ),
+        Spacer(),
+        IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.cancel,
+              size: 50,
+              color: Colors.red,
+            ))
+      ],
     );
   }
 }
